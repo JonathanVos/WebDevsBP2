7b203e84350f37c432dc0f4e825aee6f99b7a2fb
Bjorn Jacobs <bjorn_jacobs10@hotmail.com>
2016-03-08 15:13:18 +0100

    Insert script toegevoegd

---
+++ Sql_scripts/INSERT_PLACEHOLDER_DATA.sql
@@ -0,0 +1,5 @@
+use WebShop
+
+INSERT INTO Products VALUES (0, 'Stop bord', 15.95, 'Bord voor als je wilt dat mensen stoppen', 'stop')
+INSERT INTO Products VALUES (1, 'Voorangs bord', 9.95, 'Als je wilt aangeven dat mensen op een weg voorang hebben', 'voorang')
+INSERT INTO Products VALUES (2, '50 zone', 15.95, 'Bord voor als je wilt aangeven dat je in een 50km zone rijd. of als een 50 is geworden', '50zone')
--- Sql_scripts/WEBSHOP_DLL.sql
+++ Sql_scripts/WEBSHOP_DLL.sql
@@ -14,6 +14,7 @@ CREATE TABLE Products(
 	product_name VARCHAR(1000),
 	price DECIMAL(19, 2),
 	description VARCHAR(2000),
+	image_name VARCHAR(255), --where to look for the image on the server
 
 	CONSTRAINT PK_PRODUCT PRIMARY KEY(product_id),
 	CONSTRAINT CK_PRODUCT_PRICE CHECK (price >= 0)
