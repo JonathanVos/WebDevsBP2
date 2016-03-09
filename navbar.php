c700f96f846cab7036143444a05c5ae06bd1aefe
Bjorn Jacobs <bjorn_jacobs10@hotmail.com>
2016-03-09 12:05:38 +0100

    Navbar laat nu zien op welke pagina je zit

--- navbar.php
+++ navbar.php
@@ -1,15 +1,29 @@
+<?php
+
+function isBaseName($name){
+    $html = 'href="'.$name.'"';
+
+    $baseName = basename($_SERVER['PHP_SELF']);
+    if($baseName == $name){
+        $html .= ' class="active"';
+
+    }
+    return $html;
+}
+?>
 <nav>
     <ul>
-        <li><a href="index.php">Nieuws</a></li>
-        <li><a href="sale.php">Acties</a></li>
-        <li><a href="about.php" class="active" >Over ons</a></li>
-        <li><a href="vacancies.php">Vacatures</a></li>
-        <li class=""><a href="shop.php">WebShop</a>
+        <li><a  <?php echo isBaseName("index.php")?> >Nieuws</a></li>
+        <li><a <?php echo isBaseName("sale.php")?>>Acties</a></li>
+        <li><a <?php echo isBaseName("about.php")?> >Over ons</a></li>
+        <li><a <?php echo isBaseName("vacancies.php")?> >Vacatures</a></li>
+        <li ><a href="shop.php">WebShop</a>
             <ul>
-                <li><a href="shop.php">Producten</a></li>
-                <li><a href="cart.php">Winkelwagen</a></li>
-                <li><a href="payment.php">Afrekenen</a></li>
+                <li><a <?php echo isBaseName("shop.php")?> >Producten</a></li>
+                <li><a <?php echo isBaseName("cart.php")?> >Winkelwagen</a></li>
+                <li><a <?php echo isBaseName("payment.php")?> >Afrekenen</a></li>
             </ul>
         </li>
     </ul>
 </nav>
+
