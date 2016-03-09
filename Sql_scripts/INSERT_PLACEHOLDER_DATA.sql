7a9dd486f5a0d625eec6f4b1849b7572071d7f98
Bjorn Jacobs <bjorn_jacobs10@hotmail.com>
2016-03-09 10:13:52 +0100

    Update SQL

--- Sql_scripts/INSERT_PLACEHOLDER_DATA.sql
+++ Sql_scripts/INSERT_PLACEHOLDER_DATA.sql
@@ -1,5 +1,21 @@
 use WebShop
 
-INSERT INTO Products VALUES (0, 'Stop bord', 15.95, 'Bord voor als je wilt dat mensen stoppen', 'stop')
-INSERT INTO Products VALUES (1, 'Voorangs bord', 9.95, 'Als je wilt aangeven dat mensen op een weg voorang hebben', 'voorang')
-INSERT INTO Products VALUES (2, '50 zone', 15.95, 'Bord voor als je wilt aangeven dat je in een 50km zone rijd. of als een 50 is geworden', '50zone')
+DELETE FROM Products
+DELETE FROM Catogory
+DELETE FROM Product_catogories
+
+INSERT INTO Products VALUES (0, 'Stop bord', 15.95, 'Bord voor als je wilt dat mensen stoppen', 'stop', 25)
+INSERT INTO Products VALUES (1, 'Voorangs bord', 9.95, 'Als je wilt aangeven dat mensen op een weg voorang hebben', 'voorang', 30)
+INSERT INTO Products VALUES (2, '50 zone', 7.50, 'Bord voor als je wilt aangeven dat je in een 50km zone rijd. of als een 50 is geworden', '50zone', 3)
+
+INSERT INTO Catogory VALUES (0, 'Waarschuwings Borden', 'Borden voor als je de mens ergens voor moet waarschuwen')
+INSERT INTO Catogory VALUES (1, 'Informatie Borden', 'Borden voor als je de mens informatie wil geven')
+INSERT INTO Catogory VALUES (2, 'Verplichten Borden', 'Borden voor als je de mens iets moet doen')
+
+INSERT INTO Product_catogories VALUES (0, 2)
+
+SELECT * 
+FROM Products P INNER JOIN Product_catogories PC
+ON P.product_id = PC.product_id
+INNER JOIN Catogory C
+ON C.catogory_id = PC.catogory_id
--- Sql_scripts/WEBSHOP_DLL.sql
+++ Sql_scripts/WEBSHOP_DLL.sql
@@ -10,12 +10,37 @@ GO
 use WebShop
 
 CREATE TABLE Products(
-	product_id INT,
-	product_name VARCHAR(1000),
-	price DECIMAL(19, 2),
+	product_id INT NOT NULL,
+	product_name VARCHAR(1000) NOT NULL,
+	price DECIMAL(19, 2) NOT NULL,
 	description VARCHAR(2000),
 	image_name VARCHAR(255), --where to look for the image on the server
+	stock INT NOT NULL,
 
 	CONSTRAINT PK_PRODUCT PRIMARY KEY(product_id),
 	CONSTRAINT CK_PRODUCT_PRICE CHECK (price >= 0)
+)
+
+CREATE TABLE Catogory(
+	catogory_id INT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR(2000) NOT NULL,
+
+	CONSTRAINT PK_CATOGORY PRIMARY KEY (catogory_id)
+)
+
+
+CREATE TABLE Product_Catogories(
+	product_id INT NOT NULL,
+	catogory_id INT NOT NULL,
+
+	CONSTRAINT FK_PRODUCT_CATOGORIES_REF_CATOGORY FOREIGN KEY(catogory_id) 
+		REFERENCES Catogory (catogory_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+
+	CONSTRAINT FK_PRODUCT_CATOGORIES_REF_PRODUCT FOREIGN KEY(product_id)
+		REFERENCES Products (product_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
 )
