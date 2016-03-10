45ee6526fc61d3e4cc9c98adeec35fb2a2d5268f
Bjorn Jacobs <bjorn_jacobs10@hotmail.com>
2016-03-10 09:30:15 +0100

    Kijkt of de session al gestart om errors the voorkomen

--- loginForm.php
+++ loginForm.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if(!isset($_SESSION['username'])){
     echo '      <form class="login" method="post" action="checkLogin.php">
             <fieldset>
