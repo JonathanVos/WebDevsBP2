ee50f1200a4d5ae4941661f050976f0d48d20742
Bjorn Jacobs <bjorn_jacobs10@hotmail.com>
2016-03-10 15:49:39 +0100

    De gebruikers tabel bij de DLL toegevoegd

--- Sql_scripts/WEBSHOP_DLL.sql
+++ Sql_scripts/WEBSHOP_DLL.sql
@@ -29,6 +29,28 @@ CREATE TABLE Catogory(
 	CONSTRAINT PK_CATOGORY PRIMARY KEY (catogory_id)
 )
 
+CREATE TABLE Gebruikers(
+	gebruikersnaam 	VARCHAR(15)  NOT NULL,
+	voornaam       	VARCHAR(125) NOT NULL,
+	tussenvoegsel  	VARCHAR(30)  NULL,
+	achternaam     	VARCHAR(125) NOT NULL,
+	straatnaam     	VARCHAR(125) NOT NULL,
+	huisnummer     	INTEGER      NOT NULL,
+	postcode       	CHAR(6)      NOT NULL,
+	woonplaats		VARCHAR(125) NOT NULL,
+	email 			VARCHAR(50)  NOT NULL,
+	geslacht 		CHAR(1)      NOT NULL,
+	wachtwoord      VARCHAR(64)  NOT NULL,
+
+	CONSTRAINT PK_GEBRUIKER      PRIMARY KEY (gebruikersnaam),
+	CONSTRAINT CK_GEBRUIKERSNAAM CHECK (LEN(gebruikersnaam) >= 1),
+	CONSTRAINT CK_VOORNAAM       CHECK (LEN(voornaam) > 0),
+	CONSTRAINT CK_ACHTERNAAM     CHECK (LEN(achternaam) >= 0),
+	CONSTRAINT CK_WACHTWOORD     CHECK (LEN(wachtwoord) >= 4),
+	CONSTRAINT CK_POSTCODE       CHECK (postcode LIKE '[1-9][0-9][0-9][0-9][A-Z][A-Z]'),
+	CONSTRAINT CK_GESLACHT       CHECK (geslacht IN ('M', 'V')
+));
+
 
 CREATE TABLE Product_Catogories(
 	product_id INT NOT NULL,
@@ -45,9 +67,13 @@ CREATE TABLE Product_Catogories(
 		ON DELETE CASCADE,
 )
 
+GO
+
 CREATE VIEW Product_with_catogory AS
 SELECT P.product_id, product_name, P.description,P.price, image_name, stock, C.name AS catogory_name
 FROM Products P LEFT JOIN Product_Catogories PC
 ON P.product_id = PC.product_id
 LEFT JOIN Catogory C
 ON C.catogory_id = PC.catogory_id
+
+GO
