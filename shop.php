85b2144260bb27516a494146ce913f33957320d2
Bjorn Jacobs <bjorn_jacobs10@hotmail.com>
2016-03-10 11:30:07 +0100

    Kan ip catogorien filteren en zoeken

--- shop.php
+++ shop.php
@@ -1,42 +1,72 @@
 <?php
 require_once('database/productsDatabase.php');
-$content = '';
 
+$limit = 12;
+if(isset($_GET['limit']) && ctype_digit(strval($_GET['limit'])) && (int)$_GET['limit'] < 49){
+    $limit = (int)$_GET['limit'];
+}
+
+$search = "";
+if(isset($_GET['search'])){
+    $search = $_GET['search'];
+}
+
+$selectedCatogory = "";
+if(isset($_GET['catogory'])){
+    $selectedCatogory = $_GET['catogory'];
+}
+
+
+$content = '';
 //add search to the html
 $content .= '
     <div class="searchmenu">
-        <form>
-          <input type="text">
+     <form method="get" target="_self" action="shop.php">
+          <input type="text" name="search">
           <button type="submit">Search</button>
-        </form>
-
-
-        <form method="get" target="_self" action="#">
+          <br>
     '.getProductCount().' Producten | Toon
-    <select name="productcount" onchange="this.form.submit()">
-            <option value="12">12</option>
-            <option value="24">24</option>
-            <option value="48">48</option>
+    <select name="limit" onchange="this.form.submit()"  >
+            '.getOption(12, $limit).'
+            '.getOption(24, $limit).'
+            '.getOption(48, $limit).'
     </select>
     Per pagina
 </form>
-      </div>';
+</div>';
 
 //Add catogories to the html
 $content .= '<div class="categories"><ul>';
 $catogories = getCatogories();
 foreach ($catogories as $catogory) {
-    $content .= '<li><a href="#">' . $catogory['name'] . '</a></li>';
+    $content .= '<li><a href="shop.php?catogory='.$catogory['name'].'">' . $catogory['name'] . '</a></li>';
 }
 $content .= '</ul></div>';
 
 //add the product list to html
 $content .= '<div class="productslist"> <ul>';
-$products = getProducts();
-foreach ($products as $item) {
+
+$sql = "WHERE product_name LIKE '%".$search."%'";
+if($selectedCatogory != "")
+    $sql .= " AND catogory_name = '".$selectedCatogory."'";
+
+$products = getProducts($limit, $sql);
+if(count($products) == 0)
+    $content .= '<br>Geen producten gevonden.';
+foreach ($products  as $item) {
     $content .= getProductSmallHtml($item);
 }
 
 $content .= '</ul></div>';
 require 'template.php';
+
+function getOption($htmlValue, $value){
+    $html = '<option value="'.$htmlValue.'"';
+    if($value == $htmlValue){
+        $html .= ' selected="selected"';
+    }
+
+    $html .= '>'.$htmlValue.'</option>';
+    return $html;
+}
 ?>
